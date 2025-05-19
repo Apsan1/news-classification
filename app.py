f73be9d140ce6768954718cc9fb5011a938419b7
Apsan1 <apsanrana61@gmail.com>
2025-05-19 15:22:23 +0545

    Add requirements txt and use tensorflow instead of keras

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 import os
 
